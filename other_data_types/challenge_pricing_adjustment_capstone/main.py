dafbd014ec1b9951f3d07ead5a0a4b73eef9269a
RoddChames <>
2026-01-24 03:56:15 +0000

    Challenge: Pricing Adjustment Capstone

--- other_data_types/challenge_pricing_adjustment_capstone/main.py
+++ other_data_types/challenge_pricing_adjustment_capstone/main.py
@@ -4,13 +4,19 @@ grocery_inventory = {
     "Bread":("Bakery",2.99,15)
 }
 
-
-egg_price = grocery_inventory["Eggs"][1]
-print(egg_price)
-#print(grocery_inventory["Eggs"][1])
+category, price, stock = grocery_inventory["Eggs"]
+category, price, stock = grocery_inventory["Milk"]
 
 if grocery_inventory["Eggs"][1] > 5:
-    print("Eggs are too expensive, reducing the price by $1 ")
-    grocery_inventory.update({"Eggs":("Dairy",4.50,30)})
-    print(grocery_inventory["Eggs"][1])
+    print("Eggs are too expensive, reducing the price by $1.")
+    grocery_inventory["Eggs"] = (category,price -1,stock)
+    #print(grocery_inventory["Eggs"])
+
+grocery_inventory.update({"Tomatoes":("Produce",1.20,30)})
+print("Inventory after adding Tomatoes: " + str(grocery_inventory))
+
+if grocery_inventory["Milk"][2] < 10:
+    print("Milk needs to be restocked. Increasing stock by 20 units.")
+    grocery_inventory["Milk"] = (category, price, stock + 20)
+    print("Updated inventory: " + str(grocery_inventory))
     
