879807eb6c35e903f3edd33e2707851ddfcc57e6
RoddChames <>
2026-01-27 19:28:58 +0000

    Challenge: Sales and Shipments

---
+++ loops/challenge_sales_and_shipments/main.py
@@ -0,0 +1,9 @@
+# Products and their current stock
+products = [["Apples", 10], ["Bananas", 8]]
+
+# Units sold today (same order as products)
+units_sold = [3, 5]
+
+for item in range(len(products)):
+    
+    print(units_sold)
